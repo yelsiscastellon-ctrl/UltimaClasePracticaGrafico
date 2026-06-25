46c654de9ef5e7e3060eeaf3f1025d3b61eadbf5
vanessa <scarlethvgm26@gmail.com>
2026-06-24 20:55:15 -0600

    agrego proyecto.py y proyecto.md con mis datos

---
+++ proyecto.md
@@ -0,0 +1,11 @@
+# Proyecto Git
+
+Nombre: Vanessa Garay
+
+Grupo: 1
+
+Año: 2026
+
+Fecha: 23/06/2026
+
+Hora: 8:48
---
+++ proyecto.py
@@ -0,0 +1,7 @@
+# Nombre: Scarleth Vanessa Garay Martinez
+# Grupo: 1
+# Año: 2026
+# Fecha: 23/06/2026
+# Hora: 8:47
+
+print("Proyecto de control de versiones")
